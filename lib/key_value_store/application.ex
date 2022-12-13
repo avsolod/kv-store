58ca05ac50d35287832dbd55c66aa988518fbc4a
avsolod <alexei.solodkov@gmail.com>
2022-12-13 13:01:16 +0300

    initial commit

---
+++ lib/key_value_store/application.ex
@@ -0,0 +1,20 @@
+defmodule KeyValueStore.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: KeyValueStore.Worker.start_link(arg)
+      # {KeyValueStore.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: KeyValueStore.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
