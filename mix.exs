94643007cace98a5dfb80f3c4d8d283caf0d69eb
avsolod <alexei.solodkov@gmail.com>
2022-12-13 13:00:15 +0300

    initial commit

---
+++ mix.exs
@@ -0,0 +1,27 @@
+defmodule KeyValueStore.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :key_value_store,
+      version: "0.1.0",
+      elixir: "~> 1.14",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {KeyValueStore.Application, []}
+    ]
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
+    ]
+  end
+end
